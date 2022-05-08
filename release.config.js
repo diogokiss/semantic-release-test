826864840f6af6c3b4bca6498f43e15dc3fa78d2
Diogo Kiss <diogokiss@gmail.com>
2022-05-08 14:58:08 +0200

    ci: change some small miscellaneous formatting
    A few formatting and naming were changed for the release notes pull
    request comment.

--- release.config.js
+++ release.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    "branches": ["main", "semantic-release"],
     "plugins": [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
